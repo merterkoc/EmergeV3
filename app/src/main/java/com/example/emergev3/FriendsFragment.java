a15f62b243e21ea6b262cbf5861ca2e4cefe827f
merterkoc <mertyasarerkocc@gmail.com>
2021-04-14 18:08:24 +0300

    01.04.2021/2

--- app/src/main/java/com/example/emergev3/FriendsFragment.java
+++ app/src/main/java/com/example/emergev3/FriendsFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the {@link FriendsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
+public class FriendsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class LoginFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LoginFragment() {
+    public FriendsFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class LoginFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
+     * @return A new instance of fragment FriendsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    public static FriendsFragment newInstance(String param1, String param2) {
+        FriendsFragment fragment = new FriendsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -52,7 +52,6 @@ public class LoginFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-
         }
     }
 
@@ -60,6 +59,6 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_friends, container, false);
     }
 }
--- app/src/main/java/com/example/emergev3/ProfileFragment.java
+++ app/src/main/java/com/example/emergev3/ProfileFragment.java
@@ -1,18 +1,16 @@
 package com.example.emergev3;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,25 +24,72 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class ActivityProfile extends AppCompatActivity {
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends Fragment {
 
-    DatabaseReference databaseReference;
-    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    String nameView, userId = user.getUid();
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProfileFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-        Button verifyButton = findViewById(R.id.emailGonder);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.activity_profile, container, false);
 
 
-        TextView verifyMsg = findViewById(R.id.verifyMsg);
+        DatabaseReference databaseReference;
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
 
+        Button verifyButton = view.findViewById(R.id.emailGonder);
+        TextView verifyMsg = view.findViewById(R.id.verifyMsg);
+        String nameView, userId = user.getUid();
         if (!user.isEmailVerified()) {
             verifyMsg.setVisibility(View.VISIBLE);
+            verifyButton.setVisibility(View.VISIBLE);
             //resendCode.setVisibility(View.VISIBLE);
 
           /*  resendCode.setOnClickListener(new View.OnClickListener() {
@@ -66,17 +111,17 @@ public class ActivityProfile extends AppCompatActivity {
             });*/
 
         }
-       // Button resendCode = findViewById(R.id.resendCode);
-        TextView textView = findViewById(R.id.textName);
-        ImageView avatarImage = findViewById(R.id.avatarImage);
+        // Button resendCode = findViewById(R.id.resendCode);
+        TextView textView = view.findViewById(R.id.textName);
+        ImageView avatarImage = view.findViewById(R.id.avatarImage);
         databaseReference = firebaseDatabase.getReference().child("Users");
         ValueEventListener valueEventListener= new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String name = snapshot.child(""+userId+"").child("name").getValue().toString();
-               // String avatar = snapshot.child(""+userId+"").child("avatar").getValue().toString();
+                // String avatar = snapshot.child(""+userId+"").child("avatar").getValue().toString();
                 textView.setText(name);
-            //    avatarImage.setImageURI(Uri.parse(avatar));
+                //    avatarImage.setImageURI(Uri.parse(avatar));
             }
 
             @Override
@@ -85,6 +130,7 @@ public class ActivityProfile extends AppCompatActivity {
             }
         };
         databaseReference.addValueEventListener(valueEventListener);
+
         verifyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,5 +147,8 @@ public class ActivityProfile extends AppCompatActivity {
                 });
             }
         });
+        return view;
+
+
     }
 }
